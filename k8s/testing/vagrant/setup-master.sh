81001b85f9005913aa3f3f10863194835e44c25b
MinhNPH <hieunt9@example.com>
2021-04-28 12:30:24 +0700

    update code finish v1

---
+++ k8s/testing/vagrant/setup-master.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+##########################################################################################
+# SECTION 1: PREPARE
+
+# update system
+sudo -i
+sleep 1
+yum clean all
+yum -y update
+sleep 1
+
+# config timezone
+timedatectl set-timezone Asia/Ho_Chi_Minh
+
+# disable SELINUX
+setenforce 0 
+sed -i 's/enforcing/disabled/g' /etc/selinux/config
+
+# disable firewall
+systemctl stop firewalld
+systemctl disable firewalld
+
+# config hostname
+hostnamectl set-hostname node1
+
+# config file host
+cat >> "/etc/hosts" <<END
+10.124.11.17 node1
+10.124.11.28 node2
+10.124.11.38 node3 
+END
+
+# config network, config in vagrantfile in dev
+
+# Tắt swap: Nên tắt swap để kubelet hoạt động ổn định.
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+
+##########################################################################################
+# SECTION 2: INSTALL 
+
+# install docker
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+mkdir /etc/docker
+cat > /etc/docker/daemon.json <<EOF
+{
+ "exec-opts": ["native.cgroupdriver=systemd"],
+ "log-driver": "json-file",
+ "log-opts": {
+ "max-size": "100m"
+ },
+ "storage-driver": "overlay2",
+ "storage-opts": [
+   "overlay2.override_kernel_check=true"
+ ]
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+# Cài đặt kubelet, kubeadm và kubectl
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+yum install -y -q kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable kubelet
+systemctl start kubelet
+
+# sysctl
+cat >>/etc/sysctl.d/k8s.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+
+#########################################################################################
+# SECTION 3: CONFIG
+
+# Configure NetworkManager before attempting to use Calico networking.
+cat >>/etc/NetworkManager/conf.d/calico.conf<<EOF
+[keyfile]
+unmanaged-devices=interface-name:cali*;interface-name:tunl*
+EOF
+
+# Thiết lập Kubernetes Cluster
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.124.11.17
+
+# Thiết lập kubectl cho user root trên Master Node
+export KUBECONFIG=/etc/kubernetes/admin.conf
+echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> ~/.bash_profile
+
+# Cài Flannel bằng lệnh sau
+kubectl apply -f https://docs.projectcalico.org/v3.17/manifests/calico.yaml
+
+# Đến đây Master Node của Kubernetes Cluster mà chúng ta vừa khởi tạo đã sẵn sàng cho Worker Node tham gia (join) vào,
+# chúng ta có thể kiểm tra trạng thái của Master Node như sau:
+kubectl get nodes
+
